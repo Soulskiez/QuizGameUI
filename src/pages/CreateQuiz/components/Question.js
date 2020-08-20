407ca7b787bec53a37ebcc9bc24e4da851888688
Soulskiez <tylersolnikowski@gmail.com>
2020-08-19 22:53:52 -0400

    Adjusted Question component

--- src/pages/CreateQuiz/components/Question.js
+++ src/pages/CreateQuiz/components/Question.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+	accordion: {
+		marginTop: theme.spacing(4)
+	},
 	root: {
 		display: 'flex',
 		flexDirection: 'column'
@@ -18,7 +26,7 @@ const useStyles = makeStyles({
 		flexDirection: 'row',
 		alignItems: 'center'
 	}
-});
+}));
 
 function Question(props) {
 	const classes = useStyles();
@@ -28,23 +36,30 @@ function Question(props) {
 		setChecked(e.target.checked);
 	};
 	return (
-		<div className={classes.root}>
-			<TextField label={`Question Prompt ${questionNum}`} />
-			<div className={classes.isBooleanQuestionDiv}>
-				<Typography>This is a True or False Question</Typography>
-				<Checkbox checked={checked} onChange={handleChange} />
-			</div>
-			<div>
-				<div>
-					<TextField className={classes.question} label="Answer 1" />
-					<TextField className={classes.question} label="Answer 2" />
+		<Accordion className={classes.accordion}>
+			<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+				<Typography>Question 1</Typography>
+			</AccordionSummary>
+			<AccordionDetails>
+				<div className={classes.root}>
+					<TextField label={`Question Prompt ${questionNum}`} />
+					<div className={classes.isBooleanQuestionDiv}>
+						<Typography>This is a True or False Question</Typography>
+						<Checkbox checked={checked} onChange={handleChange} />
+					</div>
+					<div>
+						<div>
+							<TextField className={classes.question} label="Answer 1" />
+							<TextField className={classes.question} label="Answer 2" />
+						</div>
+						<div>
+							<TextField className={classes.question} label="Answer 3" />
+							<TextField className={classes.question} label="Answer 4" />
+						</div>
+					</div>
 				</div>
-				<div>
-					<TextField className={classes.question} label="Answer 3" />
-					<TextField className={classes.question} label="Answer 4" />
-				</div>
-			</div>
-		</div>
+			</AccordionDetails>
+		</Accordion>
 	);
 }
 
